9e7d5885a6134ef3fdf90d23b625ad085fc9a671
Tim van den Bosch <mondayrunner@users.noreply.github.com>
2022-06-11 20:08:51 +0200

    Mobile menu added

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,6 @@
 const burger = document.querySelector('#navbar-burger');
 const menu = document.querySelector('#menu');
 
-console.log(burger);
-
 burger.addEventListener('click', function() {
-    console.log('click');
-    menu.classList.toggle('hidden');
+  menu.classList.toggle('hidden');
 });
